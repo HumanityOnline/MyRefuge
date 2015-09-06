453d3806e7912feecc7618e49c5559e62e875432
Caroline Glassberg-Powell <cazcazn@gmail.com>
2015-09-06 23:59:18 +1200

    Attempt to add many family members

--- refugee/views.py
+++ refugee/views.py
@@ -7,10 +7,19 @@ from django.core.files.storage import FileSystemStorage
 from userena.forms import SignupForm
 from .forms import RefugeeSignUpBasic, FamilyMemberFormset, RefugeeSignUpAddress
 
+KEYS = ['userena', 'basic', 'family', 'address']
+FORMS = [SignupForm, RefugeeSignUpBasic, FamilyMemberFormset, RefugeeSignUpAddress]
+FORM_LIST = zip(KEYS, FORMS)
+TEMPLATES = dict(zip(KEYS, ['refugee/' + k + '.html' for k in KEYS]))
+
 # Create your views here.
 class RefugeeSignupWizard(SessionWizardView):
-    form_list = [SignupForm, RefugeeSignUpBasic, FamilyMemberFormset, RefugeeSignUpAddress]
+    form_list = FORM_LIST
     file_storage = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'refugee'))
+
     def done(self, form_list, form_dict, **kwargs):
         form_list[0].save()
         print form_dict
+
+    def get_template_names(self):
+        return [TEMPLATES[self.steps.current]]
