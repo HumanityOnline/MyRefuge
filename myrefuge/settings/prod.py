8d39f5fda1aad96668f2a0c45d3efad9e1422576
Caroline Glassberg-Powell <cazcazn@gmail.com>
2015-09-07 17:20:50 +1200

    Add userena migrations

--- myrefuge/settings/prod.py
+++ myrefuge/settings/prod.py
@@ -37,3 +37,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+MIGRATION_MODULES = {
+    'userena': 'delete_when_userena_commits.migrations'
+}
