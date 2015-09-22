6aa4160211e98bba6a3be6fdb552accbc55a377f
hoatle <hoatle@teracy.com>
2015-09-23 00:47:22 +0700

    Merge migrate, fix flat thumbnail

---
+++ apps/citizen_refuge/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('citizen_refuge', '0004_auto_20150922_1518'),
+        ('citizen_refuge', '0006_citizenspace_country'),
+    ]
+
+    operations = [
+    ]
