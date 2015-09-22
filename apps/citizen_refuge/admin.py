541bdfd1227c2f07ad41523ccc4cee1f34f44122
hoatle <hoatle@teracy.com>
2015-09-23 03:17:07 +0700

    implement search service

--- apps/citizen_refuge/admin.py
+++ apps/citizen_refuge/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
-from .models import CitizenSpace
+from .models import CitizenSpace, DateRange
 
-admin.site.register(CitizenSpace)
+
+class DateRangeInline(admin.StackedInline):
+    model = DateRange
+    extra = 1
+
+
+class CitizenSpaceAdmin(admin.ModelAdmin):
+    inlines = [
+        DateRangeInline,
+    ]
+
+admin.site.register(CitizenSpace, CitizenSpaceAdmin)
--- apps/citizen_refuge/models.py
+++ apps/citizen_refuge/models.py
@@ -1,7 +1,9 @@
+from unidecode import unidecode
+
 from django.conf import settings
 from django.db import models
 from django.contrib.gis.db import models as gis_models
-from django.contrib.gis import geos
+from django.contrib.gis import geos, measure
 
 from select_multiple_field.models import SelectMultipleField
 from address.models import AddressField
@@ -10,6 +12,13 @@ from common.helpers import APPLICATION_STATUS, CITIZEN_SPACE_ADDITIONAL, GENDER,
 from common.geo import (address_to_location, location_to_latlon, location_to_city,
                         location_to_country)
 from refugee.models import Refugee
+
+
+def _normalize_name(name):
+    """
+    convert to ascii lower case no white space only
+    """
+    return unidecode(name).lower().replace(' ', '')
 
 
 class CitizenRefuge(models.Model):
@@ -30,14 +39,31 @@ class CitizenSpaceManager(gis_models.GeoManager):
     # TODO(hoatle): implement this
     # this could help: http://www.rkblog.rk.edu.pl/w/p/shops-near-you-geographic-features-geodjango/
     # sort by distance
-    def search(self, address, date_range, guests, **kwargs):
-        """Search spaces basing on the address, date_range, guests, etc"""
+    def search(self, address, date_range, guests, distance=20000, **kwargs):
+        """Search spaces basing on the address, date_range, guests, etc
+        :param address the raw address
+        :date_range the sequence of 2 dates: start and end date
+        :guests the number of guests that a space could accommodate
+        :distance the meters from the raw address to look for, default: 10 km
+        """
+        start_date, end_date = date_range
+        query = self.filter(guests__gte=guests)
+        query = query.filter(daterange__start_date__lte=start_date)\
+                     .filter(daterange__end_date__gte=end_date)
+        # TODO(hoatle): this takes lot of time, need to improve this
         location = address_to_location(address)
         address_lat, address_lon = location_to_latlon(location)
-        city = location_to_city(location)
+        city = _normalize_name(location_to_city(location))
+        query = query.filter(city=city)
+        if city is None:
+            country = _normalize_name(location_to_country(location))
+            query = query.filter(country=country)
         current_point = geos.fromstr('POINT(%s %s)' % (address_lon, address_lat))
-
-        pass
+        distance_from_point = {'m': distance}
+        query = query.filter(location__distance_lte=(current_point,
+                                                     measure.D(**distance_from_point)))
+        query = query.distance(current_point).order_by('distance')
+        return query.distance(current_point)
 
 
 class CitizenSpace(models.Model):
@@ -69,9 +95,10 @@ class CitizenSpace(models.Model):
         point = 'POINT(%s %s)' % (lon, lat)
         self.location = geos.fromstr(point)
 
-        self.city = location_to_city(location)
-
-        self.country = location_to_country(location)
+        # save ascii lower case no white space only
+        self.city = _normalize_name(location_to_city(location))
+        # save ascii lower case no white space only
+        self.country = _normalize_name(location_to_country(location))
 
         super(CitizenSpace, self).save()
 
