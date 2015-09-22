6438e6babbf410571e1d79917f6fb9f5a90dc760
hoatle <hoatle@teracy.com>
2015-09-22 21:54:05 +0700

    set the right db engine on heroku

--- myrefuge/settings/prod.py
+++ myrefuge/settings/prod.py
@@ -4,8 +4,11 @@ from .common import *
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(),
 }
+
+# http://stackoverflow.com/questions/12538510/getting-databaseoperations-object-has-no-attribute-geo-db-type-error-when-do
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 ALLOWED_HOSTS = ['myrefuge.herokuapp.com']
 
