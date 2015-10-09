3c869a1c9fa4086e75365b7b2c7c576db29f5a44
hoatle <hoatle@teracy.com>
2015-10-09 10:43:40 +0700

    add random_coords filter

--- apps/common/templatetags/common_filters.py
+++ apps/common/templatetags/common_filters.py
@@ -1,5 +1,7 @@
 from django import template
 from django.conf import settings
+import random
+import math
 
 register = template.Library()
 
@@ -23,5 +25,23 @@ def get_list_item(list, key):
 
 @register.filter
 def display(val):
-    print(dir(val))
+    #print(dir(val))
     return val
+
+# maybe support min_distance later?
+#_min_distance = settings.RANDOM_COORDS_MIN_DISTANCE # meters
+#_max_distance = settings.RANDOM_COORDS_MAX_DISTANCE
+#ref: http://gis.stackexchange.com/questions/25877/how-to-generate-random-locations-nearby-my-location
+def random_coords(x0, y0, max_distance=None):
+    """generate random coordinates nearby a specified coordinates"""
+    if max_distance is None:
+        max_distance = settings.RANDOM_COORDS_MAX_DISTANCE
+
+    u = random.random()
+    v = random.random()
+    r = max_distance / 111300.0
+    w = r * math.sqrt(u)
+    t = 2 * math.pi * v
+    x = w * math.cos(t)
+    y = w * math.sin(t)
+    return x+x0, y+y0
