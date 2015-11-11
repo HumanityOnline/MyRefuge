05371fc8c603ca750ee243dfe57354d9bfe937b9
hoatle <hoatle@teracy.com>
2015-11-11 03:32:11 +0000

    use celery on prod only for now

--- myrefuge/__init__.py
+++ myrefuge/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
 
+from django.conf import settings
+
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app  # noqa
+# only on prod
+
+if not settings.DEBUG:
+    from .celery import app as celery_app  # noqa
